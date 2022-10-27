921dd76902204690cf07d6ab84d3e59f8339d92d
Brandon Cook <54552002+BrandonCook7@users.noreply.github.com>
2022-10-27 10:54:59 -0700

    Compile.

---
+++ Tests/Interoptopus-Test-Arrays/csharpBindings/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My.Company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interop.hello_world();
+        }
+    }
+}
