3e1cb29c920d4130574c5dd97cc1af6c8ffeca22
ScratchyPirate <john.sbur@wsu.edu>
2022-10-26 20:03:21 -0700

    Tested transfer of global constants. Test successful, globals can be transferred from rust to C# assuming its type can also be transferred

---
+++ Tests/Rust_Global_Test/target/debug/deps/interoptopus_backend_csharp-10069ddbedb0b338.d
@@ -0,0 +1,13 @@
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\interoptopus_backend_csharp-10069ddbedb0b338.rmeta: C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\lib.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\config.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\converter.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\docs.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\mod.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\dotnet.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\unity.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\testing.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\writer.rs
+
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\interoptopus_backend_csharp-10069ddbedb0b338.d: C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\lib.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\config.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\converter.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\docs.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\mod.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\dotnet.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\unity.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\testing.rs C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\writer.rs
+
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\lib.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\config.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\converter.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\docs.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\mod.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\dotnet.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\overloads\unity.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\testing.rs:
+C:\Users\john_\source\libraries\interoptopus-master\backends\csharp\src\writer.rs:
