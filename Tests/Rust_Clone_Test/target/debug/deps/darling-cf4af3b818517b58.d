a1cd1cf062e17a333d32dddffaafd64567446bdb
ScratchyPirate <john.sbur@wsu.edu>
2022-10-26 18:22:03 -0700

    Finished testing Clone trait. Added deepclone functionality since 'clone' could not be transferred into the C# interface

---
+++ Tests/Rust_Clone_Test/target/debug/deps/darling-cf4af3b818517b58.d
@@ -0,0 +1,8 @@
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\darling-cf4af3b818517b58.rmeta: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\lib.rs C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\macros_public.rs
+
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\libdarling-cf4af3b818517b58.rlib: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\lib.rs C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\macros_public.rs
+
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\darling-cf4af3b818517b58.d: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\lib.rs C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\macros_public.rs
+
+C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\lib.rs:
+C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\darling-0.13.4\src\macros_public.rs:
