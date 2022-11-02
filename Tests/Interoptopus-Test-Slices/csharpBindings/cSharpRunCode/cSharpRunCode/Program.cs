0bb6df85eb35fc27a516a639f045d9d87c43c54b
Brandon Cook <54552002+BrandonCook7@users.noreply.github.com>
2022-11-02 13:35:52 -0700

    Succesfully tested Rust Slices.

---
+++ Tests/Interoptopus-Test-Slices/csharpBindings/cSharpRunCode/cSharpRunCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using My.Company;
+
+namespace cSharpRunCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calls pattern_ffi_slice_1 and gets back size of array
+            int[] array1 = new int[] { 1, 2, 3, 4 };
+            GCHandle handle = GCHandle.Alloc(array1, GCHandleType.Pinned);
+            Sliceu32 e = new Sliceu32();
+            try
+            {
+                IntPtr pointer = handle.AddrOfPinnedObject();
+                e.data = pointer;
+                e.len = (ulong)array1.Length;
+            }
+            finally
+            {
+                if (handle.IsAllocated)
+                {
+                    handle.Free();
+                }
+            }
+            var size = Interop.pattern_ffi_slice_1(e);
+            Console.WriteLine("Size -> " + size);
+            int index = 2;
+            var variable = Interop.get_value_at_index(e, index);
+            Console.WriteLine("Variable at index " + index + " is " + variable);
+        }
+    }
+}
---
+++ Tests/Interoptopus-Test-Slices/target/debug/build/proc-macro2-c5063899f0e95538/build_script_build-c5063899f0e95538.d
@@ -0,0 +1,5 @@
+c:\Users\brand\Documents\Capstone\dell-pravegaapi\Tests\Interoptopus-Test-1-Example\target\debug\build\proc-macro2-c5063899f0e95538\build_script_build-c5063899f0e95538.exe: C:\Users\brand\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.46\build.rs
+
+c:\Users\brand\Documents\Capstone\dell-pravegaapi\Tests\Interoptopus-Test-1-Example\target\debug\build\proc-macro2-c5063899f0e95538\build_script_build-c5063899f0e95538.d: C:\Users\brand\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.46\build.rs
+
+C:\Users\brand\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.46\build.rs:
