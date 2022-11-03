540d706c3128cd86449471e37e4aabe6108033e9
ScratchyPirate <john.sbur@wsu.edu>
2022-11-02 17:07:39 -0700

    Added tests and notes for PartialEq and PartialOrd

---
+++ Tests/Rust_PartialOrd_and_PartialEQ_Test/target/debug/deps/ident_case-fc364caeb8f56cb0.d
@@ -0,0 +1,7 @@
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\ident_case-fc364caeb8f56cb0.rmeta: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\ident_case-1.0.1\src\lib.rs
+
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\libident_case-fc364caeb8f56cb0.rlib: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\ident_case-1.0.1\src\lib.rs
+
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\ident_case-fc364caeb8f56cb0.d: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\ident_case-1.0.1\src\lib.rs
+
+C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\ident_case-1.0.1\src\lib.rs:
---
+++ Tests/Rust_PartialOrd_and_PartialEQ_Test/target/debug/deps/strsim-a3b7223452dce243.d
@@ -0,0 +1,7 @@
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\strsim-a3b7223452dce243.rmeta: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\strsim-0.10.0\src\lib.rs
+
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\libstrsim-a3b7223452dce243.rlib: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\strsim-0.10.0\src\lib.rs
+
+c:\Users\john_\source\repos\CS421 Testing\target\debug\deps\strsim-a3b7223452dce243.d: C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\strsim-0.10.0\src\lib.rs
+
+C:\Users\john_\.cargo\registry\src\github.com-1ecc6299db9ec823\strsim-0.10.0\src\lib.rs:
