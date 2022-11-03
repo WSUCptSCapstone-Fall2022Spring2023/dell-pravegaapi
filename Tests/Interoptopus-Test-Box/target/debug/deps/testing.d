8bf253256b14fc37690ba5123ca4594a273f0ddb
Brandon Cook <54552002+BrandonCook7@users.noreply.github.com>
2022-11-03 10:53:41 -0700

    Tested boxes with ffi.

---
+++ Tests/Interoptopus-Test-Box/target/debug/deps/testing.d
@@ -0,0 +1,7 @@
+C:\Users\brand\Documents\Capstone\dell-pravegaapi\Tests\Interoptopus-Test-Box\target\debug\deps\testing.dll: testing.rs
+
+C:\Users\brand\Documents\Capstone\dell-pravegaapi\Tests\Interoptopus-Test-Box\target\debug\deps\libtesting.rlib: testing.rs
+
+C:\Users\brand\Documents\Capstone\dell-pravegaapi\Tests\Interoptopus-Test-Box\target\debug\deps\testing.d: testing.rs
+
+testing.rs:
