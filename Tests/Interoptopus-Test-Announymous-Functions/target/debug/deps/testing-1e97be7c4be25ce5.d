f5c8336fd577dcff439da3178b3170658902c399
Brandon Cook <54552002+BrandonCook7@users.noreply.github.com>
2022-11-09 11:04:51 -0800

    Tested Rust Closure/Anonymous Functions.

---
+++ Tests/Interoptopus-Test-Announymous-Functions/target/debug/deps/testing-1e97be7c4be25ce5.d
@@ -0,0 +1,5 @@
+C:\Users\brand\Documents\Capstone\dell-pravegaapi\Tests\Interoptopus-Test-Announymous-Functions\target\debug\deps\testing-1e97be7c4be25ce5.exe: testing.rs
+
+C:\Users\brand\Documents\Capstone\dell-pravegaapi\Tests\Interoptopus-Test-Announymous-Functions\target\debug\deps\testing-1e97be7c4be25ce5.d: testing.rs
+
+testing.rs:
